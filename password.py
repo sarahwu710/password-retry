e66ecf8deda51629dc0685f8ee50c508ab2b2ed4
Sarah Wu <s58401021@yahoo.com.tw>
2021-04-05 16:39:18 +0800

    change to while chance > 0

--- password.py
+++ password.py
@@ -1,6 +1,6 @@
 password = 'a123456'
 chance = 3 # 最多3次輸入機會
-while True:
+while chance > 0:
 	pwd = input('請輸入密碼: ')
 	if pwd == password:
 		print('登入成功!')
@@ -8,5 +8,4 @@ while True:
 	else:
 		chance = chance - 1
 		print('密碼錯誤! 還有', chance, '次機會')
-		if chance == 0:
-			break
+
