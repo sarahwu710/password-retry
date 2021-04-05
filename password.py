14aa271103e2b2a3594d0a19639850d329aed662
Sarah Wu <s58401021@yahoo.com.tw>
2021-04-05 16:23:24 +0800

    basic version done

---
+++ password.py
@@ -0,0 +1,12 @@
+password = 'a123456'
+chance = 3 # 最多3次輸入機會
+while True:
+	pwd = input('請輸入密碼: ')
+	if pwd == password:
+		print('登入成功!')
+		break # 逃出迴圈
+	else:
+		chance = chance - 1
+		print('密碼錯誤! 還有', chance, '次機會')
+		if chance == 0:
+			break
